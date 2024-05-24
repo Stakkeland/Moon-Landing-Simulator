c74bda5adb67d628f6edc94e8cd3c295451c97aa
Stakkeland <sawyerstakkeland@gmail.com>
2024-05-24 11:30:13 -0700

    foundations set for star class

--- star.h
+++ star.h
@@ -18,5 +18,19 @@
 class Star
 {
 public:
+    // Constructor
+    Star() : phase(0) {} // Initializes phase to 0
 
+    // Member function to reset the star's position and phase
+    void reset(double width, double height) {
+        
+    }
+
+    void draw(ogstream& gout) const {
+        
+    }
+
+private:
+	unsigned char phase;
+	Position pos;
 };
